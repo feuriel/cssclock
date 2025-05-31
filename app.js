c1ec4f0781b84a6ddd84327c102dccd4d21ace59
GabrielS <gabriel.somogyi.feuga@gmail.com>
2025-05-31 18:16:07 +0200

    updating portfolio link

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import DigitGroup from './DigitGroup.js';
-import HandClock from './HandClock.js';
+import DigitGroup from "./DigitGroup.js";
+import HandClock from "./HandClock.js";
 
 // Create a new Date object
 const now = new Date();
 
 // Get the current hour, minute, and second
-const now_hour = now.getHours();   // Returns the hour (0-23)
+const now_hour = now.getHours(); // Returns the hour (0-23)
 const now_minute = now.getMinutes(); // Returns the minute (0-59)
 const now_second = now.getSeconds(); // Returns the second (0-59)
 let digitGroupHour = new DigitGroup(0, 2);
@@ -13,20 +13,20 @@ let digitGroupMinute = new DigitGroup(0, 2);
 let digitGroupSecond = new DigitGroup(0, 2);
 
 digitGroupHour.setValue(now_hour);
-digitGroupHour.render(document.getElementById('content'));
-const addDot = document.createElement('span');
-addDot.innerText = ':';
-addDot.style.paddingLeft = '10px';
-addDot.style.paddingRight = '10px';
-addDot.style.fontSize = '40px';
-addDot.style.alignContent = 'center';
+digitGroupHour.render(document.getElementById("content"));
+const addDot = document.createElement("span");
+addDot.innerText = ":";
+addDot.style.paddingLeft = "10px";
+addDot.style.paddingRight = "10px";
+addDot.style.fontSize = "40px";
+addDot.style.alignContent = "center";
 const addDot2 = addDot.cloneNode(true);
-document.getElementById('content').appendChild(addDot);
+document.getElementById("content").appendChild(addDot);
 digitGroupMinute.setValue(now_minute);
-digitGroupMinute.render(document.getElementById('content'));
-document.getElementById('content').appendChild(addDot2);
+digitGroupMinute.render(document.getElementById("content"));
+document.getElementById("content").appendChild(addDot2);
 digitGroupSecond.setValue(now_second);
-digitGroupSecond.render(document.getElementById('content'));
+digitGroupSecond.render(document.getElementById("content"));
 let sec = now_second,
   min = now_minute,
   hour = now_hour;
@@ -55,7 +55,12 @@ setInterval(() => {
 }, 1000);
 
 let handClock = new HandClock(now);
-handClock.render(document.getElementById('content2'));
+handClock.render(document.getElementById("content2"));
 
-const footer = document.getElementsByTagName('footer')[0];
-footer.innerText = `© ${now.getFullYear()} Gabriel S - R Studio`;
+const footer = document.getElementsByTagName("footer")[0];
+footer.innerText = `© ${now.getFullYear()} Gabriel S.`;
+
+let copyright = `© ${new Date().getFullYear()} Gabriel Somogyi`;
+console.log(`made with ❤️ by Gabriel S.
+  https://gabriel-somogyi.vercel.app/
+  ${copyright}`);
